387c5f660b784ef21557e2c0e23c1f4b50a55414
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-05-03 11:51:12 +0530

    colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import PackageContext from "./context" 
+import Provider from "./Provider"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hello1 = () => {
+      return(
+        <div>
+             <PackageContext.Consumer>
+                 {
+                  (info)=>{
+                      return(
+                         <div>
+                            <div style={info.defaultWhite}>
+                                 <h1>Hello</h1> 
+                                 <p>How are you doing, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus quam expedita voluptatibus veniam, maxime asperiores officiis earum reprehenderit suscipit doloribus dignissimos quidem ab quas fuga sequi ex mollitia. Facere, ipsa.</p>
+                            </div>
+                              <button onClick={info.defaultRed}>Change Color</button>
+                          </div>
+                      )
+                  }
+                 }
+             </PackageContext.Consumer>
+        </div>
+      )
+}
+
+
+const App = () => {
+    return(
+        <div>
+
+            <Provider>
+                <Hello1/>
+            </Provider>
+
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Provider.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import PackageContext from "./context"; 
+
+
+
+const Provider=(props) =>{
+
+    let [color,setColor] = useState({
+        color:"white",
+        backgroundColor:"black",
+        fontSize:"20px",
+        border: "2px solid black",
+    });
+    
+    return(
+        <PackageContext.Provider value={
+            {
+              defaultWhite: color,
+              defaultRed: ()=>setColor({...color, color:"red", backgroundColor: "white"})
+
+            }
+            }>
+            {props.children}
+        </PackageContext.Provider>
+    )
+}
+
+export default Provider;
